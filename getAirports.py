454c1c5c18fd335b6695955524d8e75509447fb6
sukhmeet <sukhmeet@gmail.com>
2018-06-07 14:53:43 -0500

    fix

--- getAirports.py
+++ getAirports.py
@@ -14,18 +14,17 @@ import urllib.request
 
 def getAirports():
 
-    url_airport_ref="https://clients1.google.com/complete/search?client=flights&hl=en&gl=us&gs_rn=64&gs_ri=flights&requiredfields=regions_enabled%3Afalse%7Ctrains_enabled%3Atrue%7Cdeduplicate_cities_enabled%3Afalse%7Cairportless_locations_enabled%3Atrue&ds=flights&cp=4&gs_id=57&q=miami&callback=google.sbox.p50&gs_gbg=ubZgH31ccdKzlj7cm87O59vyZs"
+    url_airport_ref="https://clients1.google.com/complete/search?client=flights&hl=en&gl=us&gs_rn=64&gs_ri=flights&requiredfields=regions_enabled%3Afalse%7Ctrains_enabled%3Atrue%7Cdeduplicate_cities_enabled%3Afalse%7Cairportless_locations_enabled%3Atrue&ds=flights&cp=4&gs_id=57&q=airport_to_search&callback=google.sbox.p50&gs_gbg=ubZgH31ccdKzlj7cm87O59vyZs"
 
     departure_search_str = input('Enter Departure Airport: ')
-    url_airport = url_airport_ref.replace("miami", departure_search_str)
-
+    url_airport = url_airport_ref.replace("airport_to_search", departure_search_str)
+    url_airport1 = url_airport
     response = requests.get(url_airport, verify=False, stream=True)
     response = response.text.replace("google.sbox.p50 && google.sbox.p50(", "")
     response = response.replace("])", "]")
 
 
     data = json.loads(response)
-    #pprint(data)
 
     dict_airports_out = {}
     dict_airports_out['departure_airport']={}
@@ -37,7 +36,7 @@ def getAirports():
         dict = l[3]
         i += 1
         print("#" + str(i) +" : " + dict['a'], dict['d'], dict['f'])
-        #print(dict)
+
     j = int(input('Enter your choice : '))
     d = data_airports[j-1]
     dict = d[3]
@@ -45,11 +44,12 @@ def getAirports():
     dict_airports_out['departure_airport']['code']= dict['a']
 
 
+    # search for 2nd Airport
 
 
+    destination_search_str = input('Enter Destination Airport: ')
 
-    departure_search_str = input('Enter Destination Airport: ')
-    url_airport = url_airport_ref.replace("miami", departure_search_str)
+    url_airport = url_airport_ref.replace("airport_to_search", destination_search_str)
 
     response = requests.get(url_airport, verify=False, stream=True)
     response = response.text.replace("google.sbox.p50 && google.sbox.p50(", "")
@@ -57,28 +57,34 @@ def getAirports():
 
 
     data = json.loads(response)
+
     print("Select Airport # from the list below ")
     i = 0
     data_airports = data[1]
     for l in data_airports:
         dict = l[3]
         i += 1
-
+        print("#" + str(i) +" : " + dict['a'], dict['d'], dict['f'])
     j = int(input('Enter your choice : '))
     d = data_airports[j-1]
     dict = d[3]
+
+
     dict_airports_out['destination_airport']['name']= dict['a'] + "," + dict['d'] + "," + dict['f']
     dict_airports_out['destination_airport']['code']= dict['a']
 
+    # write to file
     with open('airports.json', 'w') as fp:
         json.dump(dict_airports_out, fp)
 
-
-    #pprint(dict_airports_out)
+    print("Saved Airport Data \n")
+    pprint(dict_airports_out)
 
 
 def main():
     global cmd
+    requests.packages.urllib3.disable_warnings()
     getAirports()
+    print("\nDone\n")
 
 main()
